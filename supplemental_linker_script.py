a9320864526a675db0236c84beba98033b15a7f6
netherwaves <avery@netherwaves.com>
2023-04-21 01:18:06 -0400

    update PlatformIO config

--- supplemental_linker_script.py
+++ supplemental_linker_script.py
@@ -1,12 +1,9 @@
 Import("env")
 
-import shutil
-
 print("running supplimental linker script . . .")
 env.Append(
   LINKFLAGS=[
         "-mfloat-abi=hard",
-        "-mfpu=fpv4-sp-d16",
-        "-Ilib/libDaisy/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc"
+        "-mfpu=fpv4-sp-d16"
   ]
 )
