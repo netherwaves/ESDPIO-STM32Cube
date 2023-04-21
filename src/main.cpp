9c81e5635015d526aa2139400450b34472979820
netherwaves <avery@netherwaves.com>
2023-04-21 01:18:11 -0400

    update default sketch

--- src/main.cpp
+++ src/main.cpp
@@ -12,18 +12,19 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in, AudioHandle::Interle
   for (size_t i = 0; i < size; i += 2)
   {
     float samp = osc.Process();
-    out[i] = out[i + 1] = samp * 0.05f;
+    out[i] = out[i + 1] = samp;
   }
 }
 
 int main(void)
 {
+  hw.Configure();
   hw.Init();
   hw.SetAudioBlockSize(4);
   float sample_rate = hw.AudioSampleRate();
 
   osc.Init(sample_rate);
-  osc.SetAmp(1);
+  osc.SetAmp(0.5);
   osc.SetFreq(1000);
   osc.SetWaveform(Oscillator::WAVE_SIN);
 
