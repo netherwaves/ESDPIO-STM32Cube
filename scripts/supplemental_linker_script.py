3fc2b287c06cea5bf1122a284dc30a7ed740046c
netherwaves <avery@netherwaves.com>
2023-04-22 14:46:55 -0400

    small change to linker script

--- scripts/supplemental_linker_script.py
+++ scripts/supplemental_linker_script.py
@@ -1,33 +1,34 @@
 Import("env")
-
-LD_PATH = 'lib/libDaisy/core/STM32H750IB_%s.lds'
+import os
 
 # BOOTLOADER CONFIGS
 board_config = env.BoardConfig()
 
 # manage boot type
 BOOT_TYPE = env.GetProjectOption("boot_type")
+LD_PATH = os.path.join(env.get('PROJECT_DIR'), 'lib/libDaisy/core/STM32H750IB_%s.lds' % BOOT_TYPE)
 
 if BOOT_TYPE == 'none':
-    board_config.update("build.ldscript", LD_PATH % 'flash')
+    board_config.update("build.ldscript", LD_PATH)
     board_config.update("upload.offset_address", '0x08000000')
 
 elif BOOT_TYPE == 'sram':
-    board_config.update("build.ldscript", LD_PATH % 'sram')
+    board_config.update("build.ldscript", LD_PATH)
     board_config.update("upload.offset_address", '0x90040000')
-    env.Append(CCFLAGS=['-DBOOT_APP'])
+    env.Append(CFLAGS=['-DBOOT_APP'])
 
 elif BOOT_TYPE == 'qspi':
-    board_config.update("build.ldscript", LD_PATH % 'qspi')
+    board_config.update("build.ldscript", LD_PATH)
     board_config.update("upload.maximum_size", "67108864")
     board_config.update("upload.offset_address", '0x90040000')
-    env.Append(CCFLAGS=['-DBOOT_APP'])
+    env.Append(CFLAGS=['-DBOOT_APP'])
 
 # link flags
 # otherwise there are errors. Do Not Remove !!!!!!!
 print("running supplimental linker script . . .")
 env.Append(
   LINKFLAGS=[
+        "-static",
         "-mfloat-abi=hard",
         "-mfpu=fpv4-sp-d16"
   ]
