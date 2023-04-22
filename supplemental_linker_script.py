a7e06fa5a5cd043b069a48bfc1fb3a80b481b79d
netherwaves <avery@netherwaves.com>
2023-04-22 01:15:35 -0400

    add boot_type to platformio script

--- supplemental_linker_script.py
+++ supplemental_linker_script.py
@@ -1,5 +1,30 @@
 Import("env")
 
+LD_PATH = 'lib/libDaisy/core/STM32H750IB_%s.lds'
+
+# BOOTLOADER CONFIGS
+board_config = env.BoardConfig()
+
+# manage boot type
+BOOT_TYPE = env.GetProjectOption("boot_type")
+
+if BOOT_TYPE == 'none':
+    board_config.update("build.ldscript", LD_PATH % 'flash')
+    board_config.update("upload.offset_address", '0x08000000')
+
+elif BOOT_TYPE == 'sram':
+    board_config.update("build.ldscript", LD_PATH % 'sram')
+    board_config.update("upload.offset_address", '0x90040000')
+    env.Append(CCFLAGS=['-DBOOT_APP'])
+
+elif BOOT_TYPE == 'qspi':
+    board_config.update("build.ldscript", LD_PATH % 'qspi')
+    board_config.update("upload.maximum_size", "67108864")
+    board_config.update("upload.offset_address", '0x90040000')
+    env.Append(CCFLAGS=['-DBOOT_APP'])
+
+# link flags
+# otherwise there are errors. Do Not Remove !!!!!!!
 print("running supplimental linker script . . .")
 env.Append(
   LINKFLAGS=[
