287ba7cb57dae9b268a6d80300fb1878e83b5fce
netherwaves <avery@netherwaves.com>
2023-04-23 17:27:59 -0400

    update bootloader modes sizes for PIO compilation

--- scripts/supplemental_linker_script.py
+++ scripts/supplemental_linker_script.py
@@ -8,13 +8,24 @@ board = env.BoardConfig()
 BUILD_DIR = env.get('BUILD_DIR')
 TARGET = env.get('PROGNAME')
 
-# manage boot type
+# manage boot type and update RAM + flash sizes
+# TODO: find way to display "QSPI" instead of "Flash"?
 BOOT_TYPE = env.GetProjectOption("boot_type")
 if BOOT_TYPE == 'flash':
     board.update("upload.offset_address", '0x08000000')
+    board.update("upload.maximum_size", 131072)             # 128KB
+    board.update("upload.maximum_ram_size", 524288)         # 512KB
+
 elif BOOT_TYPE in {'sram', 'qspi'}:
     board.update("upload.offset_address", '0x90040000')
     env.Append(CCFLAGS=['-DBOOT_APP'])
+
+    if BOOT_TYPE == 'sram':
+        board.update('upload.maximum_size', 491520)         # 480KB
+
+    if BOOT_TYPE == 'qspi':
+        board.update('upload.maximum_ram_size', 524288)     # 512KB
+        board.update('upload.maximum_size', 8126464)        # 7.75MB
 
 # link flags
 # otherwise there are errors. Do Not Remove !!!!!!!
@@ -35,7 +46,6 @@ env.Append(
 env.Append(
     CFLAGS=[
         '-std=gnu11',
-
     ]
 )
 env.Append(
